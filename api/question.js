bbc16e2387cd0da7c391a54cbba5cfc14aa8372f
Rakesh  Palani <144886621+Rakesh051204@users.noreply.github.com>
2026-06-03 23:11:39 +0530

    Update question.js

--- api/question.js
+++ api/question.js
@@ -1,5 +1,10 @@
 export default async function handler(req, res) {
   const { mode } = req.body;
+  export default async function handler(req, res) {
+  return res.status(200).json({
+    text: "What is overfitting in machine learning?"
+  });
+}
 
   const prompt = `
 You are a FAANG interviewer.
